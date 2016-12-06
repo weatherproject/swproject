0f44d9ac258c1b9c310ab8e09ae9f588aec1524d
JOOYEON <oneestrella1@gmail.com>
2016-12-06 19:46:19 +0900

    struct success!! -jooyeon

--- edit_textfile/please.c
+++ edit_textfile/please.c
@@ -16,13 +16,14 @@ int main()
 {
 	//char *c=0;
 	char str[500]={0,};
-	struct city A02[10]={0,};
-	struct city A01[10]={0,};
+	struct city citys[4][2][10]={0,};
+	//struct city A02[10]={0,};
+	//struct city A01[10]={0,};
 	char city_name[300];
 	char city_main[100];
 	int i=0;
 
-	
+	int flag=0;	
 	FILE *fp = fopen("weather_text.txt","rt");
 
 	if(fp == NULL)
@@ -31,85 +32,87 @@ int main()
 		return 0;
 	}
 
-	 
+
 	while(1)
 	{
-		
-		fgets(str,500,fp);
-		//fscanf(fp,"%s \n",&str);
+
+		//fgets(str,500,fp);
+		fscanf(fp,"%s \n",&str);
 		printf("%s\n",str);
 
 		//strcpy(str,"");
-		
-		
-		i++;
-		if(i>5)
+		if(strstr(str,"서울ㆍ인천ㆍ경기도"))
+		{
+			
+			strcpy(city_name,str);
 			break;
+		}
 	}
-	
 
 	while(feof(fp) ==0)
 	{
 		if(strstr(city_name,"서울ㆍ인천ㆍ경기도"))
 		{
-			fscanf(fp,"%s \n",&str);
-			strcpy(city_main,str);
-			if(strstr(city_main,"서울"))
+			if(flag==0)
 			{
 				fscanf(fp,"%s \n",&str);
-				for(i=0; i<10; i++)
+				strcpy(city_main,str);
+				if(strstr(city_main,"서울"))
 				{
-					if(strstr(str,"A02"))									//중기날씨 5일
-					{
-						fscanf(fp,"%s \n",&A02[i].date);
-						fscanf(fp,"%s \n",&A02[i].time);
-						fgets(A02[i].weather,10,fp);
-						//fscanf(fp,"%s \n",&A02[i].weather);
-						fscanf(fp,"%d \n",&A02[i].temp);
-						fscanf(fp,"%d \n",&A02[i].temp_max);
-						fscanf(fp,"%s \n",&A02[i].condition);
-					}
 					fscanf(fp,"%s \n",&str);
-				}
-				for(i=0; i<3; i++)
-				{
-					if(strstr(str,"A01"))
+					for(i=0; i<10; i++)
 					{
-						fscanf(fp,"%s \n",&A01[i].date);
-						fscanf(fp,"%s \n",&A01[i].time);
-						fgets(A01[i].weather,10,fp);
-						//fscanf(fp,"%s \n",&A02[i].weather);
-						fscanf(fp,"%d \n",&A01[i].temp);
-						fscanf(fp,"%d \n",&A01[i].temp_max);
-						fscanf(fp,"%s \n",&A01[i].condition);
+						if(strstr(str,"A02"))									//중기날씨 5일
+						{
+							fscanf(fp,"%s \n",&citys[flag][1][i].date);
+							fscanf(fp,"%s \n",&citys[flag][1][i].time);
+							fgets(citys[flag][1][i].weather,15,fp);
+							//fscanf(fp,"%s \n",&A02[i].weather);
+							fscanf(fp,"%d \n",&citys[flag][1][i].temp);
+							fscanf(fp,"%d \n",&citys[flag][1][i].temp_max);
+							fscanf(fp,"%s \n",&citys[flag][1][i].condition);
+						}
+						fscanf(fp,"%s \n",&str);
 					}
-					fscanf(fp,"%s \n",&str);
-				}
-						
+					for(i=0; i<3; i++)
+					{
+						if(strstr(str,"A01"))
+						{
+							fscanf(fp,"%s \n",&citys[flag][0][i].date);
+							fscanf(fp,"%s \n",&citys[flag][0][i].time);
+							fgets(citys[flag][0][i].weather,15,fp);
+							//fscanf(fp,"%s \n",&A02[i].weather);
+							fscanf(fp,"%d \n",&citys[flag][0][i].temp);
+							fscanf(fp,"%d \n",&citys[flag][0][i].temp_max);
+							fscanf(fp,"%s \n",&citys[flag][0][i].condition);
+						}
+						fscanf(fp,"%s \n",&str);
+					}
+				}		
 			}
 		}
 	}
-	
+	printf("----------A1------------------\n");
 	fclose(fp);
 	for(i=0; i<10; i++)
 	{
-		printf("%s	\n",A02[i].date);	
-		printf("%s	\n",A02[i].time);
-		printf("%s	\n",A02[i].weather);
-		printf("%d	\n",A02[i].temp);
-		printf("%d	\n",A02[i].temp_max);
-		printf("%s	\n",A02[i].condition);
+		printf("%s	\n",citys[flag][1][i].date);	
+		printf("%s	\n",citys[flag][1][i].time);
+		printf("%s	\n",citys[flag][1][i].weather);
+		printf("%d	\n",citys[flag][1][i].temp);
+		printf("%d	\n",citys[flag][1][i].temp_max);
+		printf("%s	\n",citys[flag][1][i].condition);
 		printf("\n");
 	}
 	printf("-----------A1시작-----------\n");
 	for(i=0; i<3; i++)
 	{
-		printf("%s	\n",A01[i].date);	
-		printf("%s	\n",A01[i].time);
-		printf("%s	\n",A01[i].weather);
-		printf("%d	\n",A01[i].temp);
-		printf("%d	\n",A01[i].temp_max);
-		printf("%s	\n",A01[i].condition);
+		printf("%s	\n",citys[flag][0][i].date);	
+		printf("%s	\n",citys[flag][0][i].time);
+		printf("%s	\n",citys[flag][0][i].weather);
+		printf("%d	\n",citys[flag][0][i].temp);
+		printf("%d	\n",citys[flag][0][i].temp_max);
+		printf("%s	\n",citys[flag][0][i].condition);
 		printf("\n");
 	}
 	return 0;
