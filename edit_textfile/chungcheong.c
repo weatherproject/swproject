0742a20bcde55b6b4060a7305b6412b6f5db99dd
JOOYEON <oneestrella1@gmail.com>
2016-12-07 16:56:28 +0900

    edit chungcheong and library -jooyeon

--- edit_textfile/chungcheong.c
+++ edit_textfile/chungcheong.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include "change.h"
 
-struct city{
-	char date[30];
-	char time[10];
-	char weather[17];
-	int temp;
-	int temp_max;
-	char condition[10];
-};
-struct city kangwon[4][2][10]={0,};
+struct city chungcheong[4][2][10]={0,};
 
 
 
-int main()
+int changechungcheong()
 {
-	//char *c=0;
 	char str[500]={0,};
-	
 	char city_name[300];
 	char city_name1[300];
 	char city_main[100]={0,};
@@ -37,19 +28,12 @@ int main()
 
 	while(1)
 	{
-
-		//fgets(str,500,fp);
 		fscanf(fp,"%s \n",&str);
-		//printf("%s\n",str);
 
-		//strcpy(str,"");
 		if(strstr(str,"충청북도"))
 		{
-			//flag=1;
 			flag=0;
 			strcpy(city_name,str);
-			//printf("%s	\n",str);
-			printf("%s	\n",city_name);
 			break;
 		}
 	}
@@ -63,7 +47,6 @@ int main()
 			{
 				fscanf(fp,"%s \n",&str);
 				strcpy(city_main,str);
-				printf("%s	\n",city_main);
 				if(!strcmp(str,"청주"))
 				{	
 					printf("%s \n",str);
@@ -73,13 +56,12 @@ int main()
 					{
 						if(strstr(str,"A02"))									//중기날씨 5일
 						{
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].date);
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].time);
-							fgets(kangwon[flag][1][i].weather,15,fp);
-							//fscanf(fp,"%s \n",&A02[i].weather);
-							fscanf(fp,"%d \n",&kangwon[flag][1][i].temp);
-							fscanf(fp,"%d \n",&kangwon[flag][1][i].temp_max);
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].condition);
+							fscanf(fp,"%s \n",&chungcheong[flag][1][i].date);
+							fscanf(fp,"%s \n",&chungcheong[flag][1][i].time);
+							fgets(chungcheong[flag][1][i].weather,15,fp);
+							fscanf(fp,"%d \n",&chungcheong[flag][1][i].temp);
+							fscanf(fp,"%d \n",&chungcheong[flag][1][i].temp_max);
+							fscanf(fp,"%s \n",&chungcheong[flag][1][i].condition);
 						}
 						fscanf(fp,"%s \n",&str);
 					}
@@ -87,13 +69,12 @@ int main()
 					{
 						if(strstr(str,"A01"))
 						{
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].date);
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].time);
-							fgets(kangwon[flag][0][i].weather,15,fp);
-							//fscanf(fp,"%s \n",&A02[i].weather);
-							fscanf(fp,"%d \n",&kangwon[flag][0][i].temp);
-							fscanf(fp,"%d \n",&kangwon[flag][0][i].temp_max);
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].condition);
+							fscanf(fp,"%s \n",&chungcheong[flag][0][i].date);
+							fscanf(fp,"%s \n",&chungcheong[flag][0][i].time);
+							fgets(chungcheong[flag][0][i].weather,15,fp);
+							fscanf(fp,"%d \n",&chungcheong[flag][0][i].temp);
+							fscanf(fp,"%d \n",&chungcheong[flag][0][i].temp_max);
+							fscanf(fp,"%s \n",&chungcheong[flag][0][i].condition);
 						}
 						fscanf(fp,"%s \n",&str);
 					}
@@ -101,7 +82,7 @@ int main()
 				}		
 			}
 			
-		}/*
+		}
 		if(strstr(city_name,"대전ㆍ세종ㆍ충청남도"))
 		{
 			if(flag==1)
@@ -116,13 +97,12 @@ int main()
 					{
 						if(strstr(str,"A02"))									//중기날씨 5일
 						{
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].date);
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].time);
-							fgets(kangwon[flag][1][i].weather,15,fp);
-							//fscanf(fp,"%s \n",&A02[i].weather);
-							fscanf(fp,"%d \n",&kangwon[flag][1][i].temp);
-							fscanf(fp,"%d \n",&kangwon[flag][1][i].temp_max);
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].condition);
+							fscanf(fp,"%s \n",&chungcheong[flag][1][i].date);
+							fscanf(fp,"%s \n",&chungcheong[flag][1][i].time);
+							fgets(chungcheong[flag][1][i].weather,15,fp);
+							fscanf(fp,"%d \n",&chungcheong[flag][1][i].temp);
+							fscanf(fp,"%d \n",&chungcheong[flag][1][i].temp_max);
+							fscanf(fp,"%s \n",&chungcheong[flag][1][i].condition);
 						}
 						fscanf(fp,"%s \n",&str);
 					}
@@ -130,13 +110,12 @@ int main()
 					{
 						if(strstr(str,"A01"))
 						{
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].date);
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].time);
-							fgets(kangwon[flag][0][i].weather,15,fp);
-							//fscanf(fp,"%s \n",&A02[i].weather);
-							fscanf(fp,"%d \n",&kangwon[flag][0][i].temp);
-							fscanf(fp,"%d \n",&kangwon[flag][0][i].temp_max);
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].condition);
+							fscanf(fp,"%s \n",&chungcheong[flag][0][i].date);
+							fscanf(fp,"%s \n",&chungcheong[flag][0][i].time);
+							fgets(chungcheong[flag][0][i].weather,15,fp);
+							fscanf(fp,"%d \n",&chungcheong[flag][0][i].temp);
+							fscanf(fp,"%d \n",&chungcheong[flag][0][i].temp_max);
+							fscanf(fp,"%s \n",&chungcheong[flag][0][i].condition);
 						}
 						fscanf(fp,"%s \n",&str);
 					}
@@ -147,24 +126,20 @@ int main()
 			{
 				fscanf(fp,"%s \n",&str);
 				strcpy(city_main,str);
-				//printf("in the flag=0\n");
-				//printf("%s	\n",city_main);
 				if(!strcmp(str,"서산"))
 				{	
-					//printf("%s \n",str);
 					strcpy(city_save_name[flag],city_main);
 					fscanf(fp,"%s \n",&str);
 					for(i=0; i<10; i++)
 					{
 						if(strstr(str,"A02"))									//중기날씨 5일
 						{
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].date);
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].time);
-							fgets(kangwon[flag][1][i].weather,15,fp);
-							//fscanf(fp,"%s \n",&A02[i].weather);
-							fscanf(fp,"%d \n",&kangwon[flag][1][i].temp);
-							fscanf(fp,"%d \n",&kangwon[flag][1][i].temp_max);
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].condition);
+							fscanf(fp,"%s \n",&chungcheong[flag][1][i].date);
+							fscanf(fp,"%s \n",&chungcheong[flag][1][i].time);
+							fgets(chungcheong[flag][1][i].weather,15,fp);
+							fscanf(fp,"%d \n",&chungcheong[flag][1][i].temp);
+							fscanf(fp,"%d \n",&chungcheong[flag][1][i].temp_max);
+							fscanf(fp,"%s \n",&chungcheong[flag][1][i].condition);
 						}
 						fscanf(fp,"%s \n",&str);
 					}
@@ -172,62 +147,58 @@ int main()
 					{
 						if(strstr(str,"A01"))
 						{
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].date);
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].time);
-							fgets(kangwon[flag][0][i].weather,15,fp);
-							//fscanf(fp,"%s \n",&A02[i].weather);
-							fscanf(fp,"%d \n",&kangwon[flag][0][i].temp);
-							fscanf(fp,"%d \n",&kangwon[flag][0][i].temp_max);
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].condition);
+							fscanf(fp,"%s \n",&chungcheong[flag][0][i].date);
+							fscanf(fp,"%s \n",&chungcheong[flag][0][i].time);
+							fgets(chungcheong[flag][0][i].weather,15,fp);
+							fscanf(fp,"%d \n",&chungcheong[flag][0][i].temp);
+							fscanf(fp,"%d \n",&chungcheong[flag][0][i].temp_max);
+							fscanf(fp,"%s \n",&chungcheong[flag][0][i].condition);
 						}
 						fscanf(fp,"%s \n",&str);
 					}
 					flag=3;
 				}		
 			}
-		}
-		else if(flag==3)
-		{
-				fscanf(fp,"%s \n",&str);
-				strcpy(city_main,str);
-				//printf("in the flag=0\n");
-				//printf("%s	\n",city_main);
-				if(!strcmp(str,"세종"))
-				{	
-					//printf("%s \n",str);
-					strcpy(city_save_name[flag],city_main);
+			else if(flag==3)
+			{
 					fscanf(fp,"%s \n",&str);
-					for(i=0; i<10; i++)
-					{
-						if(strstr(str,"A02"))									//중기날씨 5일
-						{
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].date);
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].time);
-							fgets(kangwon[flag][1][i].weather,15,fp);
-							//fscanf(fp,"%s \n",&A02[i].weather);
-							fscanf(fp,"%d \n",&kangwon[flag][1][i].temp);
-							fscanf(fp,"%d \n",&kangwon[flag][1][i].temp_max);
-							fscanf(fp,"%s \n",&kangwon[flag][1][i].condition);
-						}
+					strcpy(city_main,str);
+					if(!strcmp(str,"세종"))
+					{	
+						//printf("%s \n",str);
+						strcpy(city_save_name[flag],city_main);
 						fscanf(fp,"%s \n",&str);
-					}
-					for(i=0; i<3; i++)
-					{
-						if(strstr(str,"A01"))
+						for(i=0; i<10; i++)
 						{
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].date);
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].time);
-							fgets(kangwon[flag][0][i].weather,15,fp);
-							//fscanf(fp,"%s \n",&A02[i].weather);
-							fscanf(fp,"%d \n",&kangwon[flag][0][i].temp);
-							fscanf(fp,"%d \n",&kangwon[flag][0][i].temp_max);
-							fscanf(fp,"%s \n",&kangwon[flag][0][i].condition);
+							if(strstr(str,"A02"))									//중기날씨 5일
+							{
+								fscanf(fp,"%s \n",&chungcheong[flag][1][i].date);
+								fscanf(fp,"%s \n",&chungcheong[flag][1][i].time);
+								fgets(chungcheong[flag][1][i].weather,15,fp);
+								fscanf(fp,"%d \n",&chungcheong[flag][1][i].temp);
+								fscanf(fp,"%d \n",&chungcheong[flag][1][i].temp_max);
+								fscanf(fp,"%s \n",&chungcheong[flag][1][i].condition);
+							}
+							fscanf(fp,"%s \n",&str);
 						}
-						fscanf(fp,"%s \n",&str);
-					}
-				}		
+						for(i=0; i<3; i++)
+						{
+							if(strstr(str,"A01"))
+							{
+								fscanf(fp,"%s \n",&chungcheong[flag][0][i].date);
+								fscanf(fp,"%s \n",&chungcheong[flag][0][i].time);
+								fgets(chungcheong[flag][0][i].weather,15,fp);
+								fscanf(fp,"%d \n",&chungcheong[flag][0][i].temp);
+								fscanf(fp,"%d \n",&chungcheong[flag][0][i].temp_max);
+								fscanf(fp,"%s \n",&chungcheong[flag][0][i].condition);
+							}
+							fscanf(fp,"%s \n",&str);
+						}
+						break;
+					}		
+			
 			}
-
+		}
 		if(!strcmp(str,"충청북도"))
 			continue;
 		else if(!strcmp(str,"대전ㆍ세종ㆍ충청남도"))
@@ -240,7 +211,7 @@ int main()
 			fscanf(fp,"%s \n",&str);
 			strcpy(city_name,str);
 		}
-
+		
 
 	}
 
@@ -248,34 +219,5 @@ int main()
 
 	fclose(fp);
 
-
-	for(k=0; k<4; k++)
-	{
-		printf("-----------A2시작-----------\n");
-		for(i=0; i<10; i++)
-		{
-			printf("%s	\n",kangwon[k][1][i].date);	
-			printf("%s	\n",kangwon[k][1][i].time);
-			printf("%s	\n",kangwon[k][1][i].weather);
-			printf("%d	\n",kangwon[k][1][i].temp);
-			printf("%d	\n",kangwon[k][1][i].temp_max);
-			printf("%s	\n",kangwon[k][1][i].condition);
-			printf("\n");
-		}
-	
-		printf("-----------A1시작-----------\n");
-		for(i=0; i<3; i++)
-		{
-			printf("%s	\n",kangwon[k][0][i].date);	
-			printf("%s	\n",kangwon[k][0][i].time);
-			printf("%s	\n",kangwon[k][0][i].weather);
-			printf("%d	\n",kangwon[k][0][i].temp);
-			printf("%d	\n",kangwon[k][0][i].temp_max);
-			printf("%s	\n",kangwon[k][0][i].condition);
-			printf("\n");
-		}
-	}
-
 	return 0;
-
 }
--- edit_textfile/main.c
+++ edit_textfile/main.c
@@ -37,31 +37,31 @@ int main()
 	}
 */
 
-	changekangwon();	
-
-	for(k=0; k<3; k++)
+//	changekangwon();	
+	changechungcheong();
+	for(k=0; k<4; k++)
 	{
 		printf("-----------A2시작-----------\n");
 		for(i=0; i<10; i++)
 		{
-			printf("%s	\n",kangwon[k][1][i].date);	
-			printf("%s	\n",kangwon[k][1][i].time);
-			printf("%s	\n",kangwon[k][1][i].weather);
-			printf("%d	\n",kangwon[k][1][i].temp);
-			printf("%d	\n",kangwon[k][1][i].temp_max);
-			printf("%s	\n",kangwon[k][1][i].condition);
+			printf("%s	\n",chungcheong[k][1][i].date);	
+			printf("%s	\n",chungcheong[k][1][i].time);
+			printf("%s	\n",chungcheong[k][1][i].weather);
+			printf("%d	\n",chungcheong[k][1][i].temp);
+			printf("%d	\n",chungcheong[k][1][i].temp_max);
+			printf("%s	\n",chungcheong[k][1][i].condition);
 			printf("\n");
 		}
 	
 		printf("-----------A1시작-----------\n");
 		for(i=0; i<3; i++)
 		{
-			printf("%s	\n",kangwon[k][0][i].date);	
-			printf("%s	\n",kangwon[k][0][i].time);
-			printf("%s	\n",kangwon[k][0][i].weather);
-			printf("%d	\n",kangwon[k][0][i].temp);
-			printf("%d	\n",kangwon[k][0][i].temp_max);
-			printf("%s	\n",kangwon[k][0][i].condition);
+			printf("%s	\n",chungcheong[k][0][i].date);	
+			printf("%s	\n",chungcheong[k][0][i].time);
+			printf("%s	\n",chungcheong[k][0][i].weather);
+			printf("%d	\n",chungcheong[k][0][i].temp);
+			printf("%d	\n",chungcheong[k][0][i].temp_max);
+			printf("%s	\n",chungcheong[k][0][i].condition);
 			printf("\n");
 		}
 	}
