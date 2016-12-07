f8b6d7a95fd9d2e2f7a42f5b83a77ff3cd8bf1ba
JOOYEON <oneestrella1@gmail.com>
2016-12-07 14:10:47 +0900

    make header, library -jooyeon

---
+++ edit_textfile/seoulgyeonggi.h
@@ -0,0 +1,20 @@
+
+extern struct city{
+	char date[30];
+	char time[10];
+	char weather[15];
+	int temp;
+	int temp_max;
+	char condition[10];
+};
+
+
+extern struct city seoul[4][2][10];
+extern struct city kangwon[4][2][10];
+
+int changeseoul();
+
+
+
+
+
