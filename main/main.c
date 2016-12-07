62faebfe34ad72e8afa7581c05e333209c2ddab2
JOOYEON <oneestrella1@gmail.com>
2016-12-07 20:38:01 +0900

    make makefile in main directory -jooyeon

--- main/main.c
+++ main/main.c
@@ -1,36 +1,74 @@
-#nclude "../edit_textfile/seoulgyeonggi.h"
+#include <stdio.h>
+#include "../edit_textfile/change.h"
 
-void main()
+//struct city seoul[4][2][10];
+
+int main()
 {
-	int k,i;
-
-	for(k=0; k<4; k++)
+	int i;
+	int k=0;
+//	changeseoul();
+//	changekangwon();
+/*	for(k=0; k<4; k++)
 	{
-		printf("%s 	\n",city_save_name[k]);
+		printf("-----------A2시작-----------\n");
+		for(i=0; i<10; i++)
+		{
+			printf("%s	\n",seoul[k][1][i].date);	
+			printf("%s	\n",seoul[k][1][i].time);
+			printf("%s	\n",seoul[k][1][i].weather);
+			printf("%d	\n",seoul[k][1][i].temp);
+			printf("%d	\n",seoul[k][1][i].temp_max);
+			printf("%s	\n",seoul[k][1][i].condition);
+			printf("\n");
+		}
+	
+		printf("-----------A1시작-----------\n");
+		for(i=0; i<3; i++)
+		{
+			printf("%s	\n",seoul[k][0][i].date);	
+			printf("%s	\n",seoul[k][0][i].time);
+			printf("%s	\n",seoul[k][0][i].weather);
+			printf("%d	\n",seoul[k][0][i].temp);
+			printf("%d	\n",seoul[k][0][i].temp_max);
+			printf("%s	\n",seoul[k][0][i].condition);
+			printf("\n");
+		}
+	}
+*/
+
+//	changekangwon();	
+//	changechungcheong();
+//	changejeonla();
+//	changegyeongsang();
+	changejeju();
+	for(k=0; k<2; k++)
+	{
 		
 		printf("-----------A2시작-----------\n");
 		for(i=0; i<10; i++)
 		{
-			printf("%s	\n",citys[k][1][i].date);	
-			printf("%s	\n",citys[k][1][i].time);
-			printf("%s	\n",citys[k][1][i].weather);
-			printf("%d	\n",citys[k][1][i].temp);
-			printf("%d	\n",citys[k][1][i].temp_max);
-			printf("%s	\n",citys[k][1][i].condition);
+			printf("%s	\n",jeju[k][1][i].date);	
+			printf("%s	\n",jeju[k][1][i].time);
+			printf("%s	\n",jeju[k][1][i].weather);
+			printf("%d	\n",jeju[k][1][i].temp);
+			printf("%d	\n",jeju[k][1][i].temp_max);
+			printf("%s	\n",jeju[k][1][i].condition);
 			printf("\n");
 		}
+	
 		printf("-----------A1시작-----------\n");
 		for(i=0; i<3; i++)
 		{
-			printf("%s	\n",citys[k][0][i].date);	
-			printf("%s	\n",citys[k][0][i].time);
-			printf("%s	\n",citys[k][0][i].weather);
-			printf("%d	\n",citys[k][0][i].temp);
-			printf("%d	\n",citys[k][0][i].temp_max);
-			printf("%s	\n",citys[k][0][i].condition);
+			printf("%s	\n",jeju[k][0][i].date);	
+			printf("%s	\n",jeju[k][0][i].time);
+			printf("%s	\n",jeju[k][0][i].weather);
+			printf("%d	\n",jeju[k][0][i].temp);
+			printf("%d	\n",jeju[k][0][i].temp_max);
+			printf("%s	\n",jeju[k][0][i].condition);
 			printf("\n");
 		}
-
-		printf("\n\n");
 	}
+
+	return 0;
 }
